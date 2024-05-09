959daa5c7197eaf9e5995d2d48cb872d810094f1
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-05-09 19:53:28 +0200

    Now I like the file downloader

--- src/main/java/dev/tonimatas/DownloadFiles.java
+++ src/main/java/dev/tonimatas/DownloadFiles.java
@@ -4,12 +4,20 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.*;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DecimalFormat;
 
 public class DownloadFiles {
-    public static final String fileURL = "https://piston-data.mojang.com/v1/objects/0b4dba049482496c507b2387a73a913230ebbd76/server.txt";
+    public static final String fileURL = "https://cdimage.ubuntu.com/kubuntu/releases/24.04/release/kubuntu-24.04-desktop-amd64.iso";
+    private static final long everyMillis = 1000;
+    private static final DecimalFormat oneDecimalFormat = new DecimalFormat("#.#");
+    private static boolean finished = false;
 
     public static void main(String[] args) {
+        
+        
         try {
             URL url = URI.create(fileURL).toURL();
             URLConnection connection = url.openConnection();
@@ -26,17 +34,42 @@ public class DownloadFiles {
 
             String fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1);
             String saveFilePath = "downloads" + File.separator + fileName;
+            File resultFile = new File(saveFilePath);
+            
+            new Thread(() -> {
+                double oldFileSizeInMB = 0;
+                int duration = 0;
+                
+                System.out.println("Downloading " + fileName + " to " + saveFilePath);
+                
+                while (!finished) {
+                    try {
+                        Thread.sleep(everyMillis);
 
+                        double fileSizeInMB = (double) resultFile.length() / 1048576;
+                        double diff = fileSizeInMB - oldFileSizeInMB;
+                        oldFileSizeInMB = fileSizeInMB;
+                        
+                        System.out.print("\r" + oneDecimalFormat.format(diff) + " MB/S | Total: " + oneDecimalFormat.format(fileSizeInMB) + " MB | " + duration + " seconds");
+                        
+                        duration++;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
             try (InputStream inputStream = connection.getInputStream();
                  FileOutputStream outputStream = new FileOutputStream(saveFilePath)) {
-
+                
                 byte[] buffer = new byte[1024];
                 int bytesRead;
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     outputStream.write(buffer, 0, bytesRead);
                 }
+                finished = true;
+                double fileSizeInMB = (double) resultFile.length() / 1048576;
 
-                System.out.println("File downloaded to: " + saveFilePath);
+                System.out.println("File downloaded to: " + saveFilePath + " | " + oneDecimalFormat.format(fileSizeInMB) + " MB");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
